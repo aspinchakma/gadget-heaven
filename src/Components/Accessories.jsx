72c9900de711727e621b19618baedda8cd7d098b
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-02 16:11:27 +0600

    accessories section done

--- src/Components/Accessories.jsx
+++ src/Components/Accessories.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../Utilities/Context";
+import Product from "./Product";
+
 const Accessories = () => {
+  const data = useContext(ProductsContext);
+  const accessories = [...data].filter((p) =>
+    p.category.includes("accessories")
+  );
+  console.log(accessories);
   return (
-    <div>
-      <h3>This is accessories</h3>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {accessories.map((p) => (
+        <Product product={p} key={p.id} />
+      ))}
     </div>
   );
 };
