b994b3bdf1e12bfb300f6e581f04ad4ddf100dcc
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-02 16:04:06 +0600

    laptop section finished

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -4,7 +4,6 @@ import Product from "./Product";
 
 const AllProducts = () => {
   const data = useContext(ProductsContext);
-  console.log(data);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
       {data.map((p) => (
--- src/Components/Laptops.jsx
+++ src/Components/Laptops.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { ProductsContext } from "../Utilities/Context";
+import Product from "./Product";
+
 const Laptops = () => {
+  const data = useContext(ProductsContext);
+  const laptopsData = [...data].filter((p) => p.category.includes("laptop"));
   return (
-    <div>
-      <h2>This is Laptops</h2>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {laptopsData.map((p) => (
+        <Product product={p} key={p.id} />
+      ))}
     </div>
   );
 };
