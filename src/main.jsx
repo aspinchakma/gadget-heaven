8432edcc7c77f4160a56c026f80070ffc641fe68
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-02 11:10:36 +0600

    basic setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h3>Hello bro</h3>
+  </StrictMode>
+);
