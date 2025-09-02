c50cf2a35ecf6b3e473f3f2fcebb65849376d23e
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-02 16:14:45 +0600

    smart watch section done

--- src/Components/SmartWatch.jsx
+++ src/Components/SmartWatch.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { ProductsContext } from "../Utilities/Context";
+import Product from "./Product";
+
 const SmartWatch = () => {
+  const data = useContext(ProductsContext);
+  const smartWatch = [...data].filter((p) =>
+    p.category.includes("smart watch")
+  );
+  console.log(smartWatch);
   return (
-    <div>
-      <h2>This is Smart Watch</h2>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {smartWatch.map((p) => (
+        <Product product={p} key={p.id} />
+      ))}
     </div>
   );
 };
