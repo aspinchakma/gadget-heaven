53091d4aa49326be1458be7e807cbdeb7b839421
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-03 13:13:10 +0600

    adding modal in cart section

--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { FaFilter } from "react-icons/fa";
-import { useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { loadDataLS } from "../Utilities/DataLS";
 import SingleProductForCart from "./SingleProductForCart";
 
@@ -8,11 +8,12 @@ const Carts = () => {
   const [products, setProducts] = useState([]);
   const [isSorted, setIsSorted] = useState(false);
   // [setCartNumber, setWishList]
-  const [setCartNumber, ...rest] = useOutletContext();
+  const setCartNumber = useOutletContext()[0];
 
   const [forSortingData, setForSortingData] = useState([]);
 
   const [total, setTotal] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const data = loadDataLS();
@@ -58,6 +59,18 @@ const Carts = () => {
     }
     setIsSorted(!isSorted);
   };
+
+  const handlePurchaseButton = () => {
+    document.getElementById("my_modal_1").showModal();
+    localStorage.removeItem("cart");
+
+    setProducts([]);
+    setCartNumber(0);
+  };
+  const handleCloseButton = () => {
+    setTotal(0);
+    navigate("/");
+  };
   return (
     <div>
       <div className="flex justify-between items-center flex-col lg:flex-row">
@@ -81,7 +94,11 @@ const Carts = () => {
               <FaFilter />
             </button>
           )}
-          <button className="bg-[#9538e2] text-white text-[17px] px-3 py-2 rounded-lg inline-block">
+          <button
+            onClick={handlePurchaseButton}
+            className="bg-[#9538e2] text-white text-[17px] px-3 py-2 rounded-lg inline-block cursor-pointer"
+            disabled={products.length === 0}
+          >
             Purchase
           </button>
         </div>
@@ -101,6 +118,28 @@ const Carts = () => {
           ""
         )}
       </div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <img
+            src="https://i.ibb.co.com/v6p1J0Vb/Group.png"
+            className="h-[80px] mx-auto"
+            alt=""
+          />
+          <div className="text-center lg:px-7 py-3">
+            <h3 className="text-[22px] font-bold mb-2">Payment Successfully</h3>
+            <p className="text-[#6b6b6f]">Thanks for purchasing.</p>
+            <p className="text-[#6b6b6f] mt-2 mb-4">Total:{total}</p>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button onClick={handleCloseButton} className="btn w-full">
+                Close
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
