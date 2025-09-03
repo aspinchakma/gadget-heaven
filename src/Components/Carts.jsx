c7cd2c85230649be5a004c195ee5529a36541683
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-03 10:16:17 +0600

    cart functionality done

--- src/Components/Carts.jsx
+++ src/Components/Carts.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { loadDataLS } from "../Utilities/DataLS";
+
 const Carts = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const data = loadDataLS();
+    const loadDataSever = async () => {
+      const url = `https://aspinchakma.github.io/api-for-practice/techProducts.json`;
+      const res = await fetch(url);
+      const result = await res.json();
+      const filteredData = data.map((id) => result.find((p) => p.id == id));
+      setProducts(filteredData);
+    };
+
+    loadDataSever();
+  }, []);
+
   return (
     <div>
       <h3>This is cart</h3>
