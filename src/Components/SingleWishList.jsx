ff648b502abf240fc51728f03fe27ce29627b7cd
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-03 13:57:27 +0600

    add to cart button funcionalities added

---
+++ src/Components/SingleWishList.jsx
@@ -0,0 +1,35 @@
+import { MdDeleteForever } from "react-icons/md";
+
+const SingleWishList = ({ product, handleAddToCart }) => {
+  const { id, img, title, brand, description, price } = product;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 shadow-lg my-3 p-[20px] rounded-lg">
+      <img
+        className="w-full object-cover h-[160px] rounded-lg"
+        src={img}
+        alt=""
+      />
+      <div className="lg:col-span-3">
+        <h3 className="text-[22px] font-bold">{title}</h3>
+        <p className="font-bold text-[14px] text-green-500">({brand})</p>
+        <p className="my-2 text-[#6b6b6f] text-[15px]">
+          {description?.slice(0, 70)}
+        </p>
+        <p className="font-bold text-[18px] mb-2">Price: ${price}</p>
+        <button
+          onClick={() => handleAddToCart(id)}
+          className="cursor-pointer text-[15px] bg-[#8d36d6] font-normal px-3 py-2 rounded-lg text-white"
+        >
+          Add To Cart
+        </button>
+      </div>
+      <div>
+        <button className="p-2 border-[#ff0000] border-2 rounded-full w-[40px] h-[40px] flex items-center justify-center">
+          <MdDeleteForever className="text-[22px] text-[#ff0000] font-bold inline-block cursor-pointer" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleWishList;
--- src/Components/WishList.jsx
+++ src/Components/WishList.jsx
@@ -1,7 +1,61 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { loadDataLS, loadDataWishList } from "../Utilities/DataLS";
+import SingleWishList from "./SingleWishList";
+
 const WishList = () => {
+  const [data, setData] = useState([]);
+  const [setCartList, setWishlist] = useOutletContext();
+  useEffect(() => {
+    const dataWishList = loadDataWishList();
+    const loadDataSever = async () => {
+      const url = `https://aspinchakma.github.io/api-for-practice/techProducts.json`;
+      const res = await fetch(url);
+      const result = await res.json();
+      const filteredData = dataWishList.map((id) =>
+        result.find((p) => p.id == id)
+      );
+      setData(filteredData);
+    };
+
+    loadDataSever();
+  }, []);
+  const handleAddToCart = (id) => {
+    const filteredData = [...data].filter((product) => product.id != id);
+
+    // updating cart data
+    const cartData = loadDataLS();
+    cartData.push(id);
+    localStorage.setItem("cart", JSON.stringify(cartData));
+    setCartList(cartData.length);
+
+    // updation wish list data
+    const wishListDataLS = loadDataWishList();
+    const filteredWishList = wishListDataLS.filter((pID) => pID != id);
+    localStorage.setItem("wish-list", JSON.stringify(filteredWishList));
+    setWishlist(filteredWishList.length);
+
+    // set data to show ui
+    setData(filteredData);
+  };
   return (
-    <div>
-      <h3>This is wish list</h3>
+    <div className="text-[19px] font-bold py-4 lg:py-8">
+      <h2 className="font-bold mb-4">WishList</h2>
+      <div>
+        {data.length ? (
+          <div>
+            {data.map((product) => (
+              <SingleWishList
+                handleAddToCart={handleAddToCart}
+                product={product}
+                key={product.id}
+              />
+            ))}
+          </div>
+        ) : (
+          <h3>hello bro</h3>
+        )}
+      </div>
     </div>
   );
 };
