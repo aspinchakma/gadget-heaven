2cccaf9c31e91d34713c80f2991f10e86299710b
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-03 22:01:34 +0600

    banner basic design done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Outlet, useOutletContext } from "react-router-dom";
+import HomeBanner from "./HomeBanner";
 import ProductsContainerSideBar from "./ProductsContainerSideBar";
 
 const Home = () => {
@@ -10,14 +11,19 @@ const Home = () => {
 
   const [setCartNumber, setWishList] = useOutletContext();
   return (
-    <div className="lg:w-[80%] w-[90%] mx-auto">
-      <h2 className="mb-10">This is HOme</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
-        <div>
-          <ProductsContainerSideBar />
-        </div>
-        <div className="lg:col-span-5 ">
-          <Outlet context={[setCartNumber, setWishList]} />
+    <div>
+      <HomeBanner />
+      <div className="lg:w-[80%] w-[90%] mx-auto">
+        <h2 className="text-xl lg:text-3xl font-bold text-center mb-4">
+          Explore Cutting-Edge Gadgets
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-6 gap-6">
+          <div>
+            <ProductsContainerSideBar />
+          </div>
+          <div className="lg:col-span-5 ">
+            <Outlet context={[setCartNumber, setWishList]} />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/Components/HomeBanner.jsx
@@ -0,0 +1,30 @@
+const HomeBanner = () => {
+  return (
+    <div className="relative mb-[170px] lg:mb-[520px]">
+      <div className="text-center bg-[#9538e2] text-white  py-[188px] lg:w-[98%] mx-auto rounded-lg w-[95%]">
+        <div className="w-[90%] lg:w-[80%] mx-auto">
+          <h2 className="text-3xl lg:text-5xl font-bold max-w-[1100px] mx-auto  text-white ">
+            Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+          </h2>
+          <p className="max-w-[796px] mx-auto py-5">
+            Explore the latest gadgets that will take your experience to the
+            next level. From smart devices to the coolest accessories, we have
+            it all!
+          </p>
+          <button className="bg-white text-[#9137dd] font-bold px-5 py-2 rounded-lg cursor-pointer">
+            Shop Now
+          </button>
+        </div>
+      </div>
+      <div className="lg:w-[60%] w-[90%] mx-auto border-2 border-[#fefeff] p-4 rounded-lg backdrop-blur-lg absolute top-[75%] left-[50%] -translate-x-[50%]">
+        <img
+          className="max-h-[563px] w-full rounded-lg"
+          src="https://i.ibb.co.com/MYkR2Kx/banner.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
