b804b4348807ff05f37e8aadec3fb435e0b4decd
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-04 00:14:12 +0600

    design not found page

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,7 +1,24 @@
+import { MdHomeFilled } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      <h3>This is Not Found</h3>
+    <div className="h-screen bg-[#131722] text-[#c7ccd0] flex items-center justify-center">
+      <div className="text-center">
+        <h3 className="text-[120px] leading-[140px] lg:text-[200px] lg:leading-[200px] font-bold">
+          404
+        </h3>
+        <p className="text-[20px] lg:text-[40px] mb-6">
+          Oops something went wrong
+        </p>
+        <NavLink
+          to={`/`}
+          className={`flex items-center gap-3 w-fit mx-auto border-2 font-bold text-[18px] border-[#252934] rounded-lg px-4 py-2`}
+        >
+          Back To Home
+          <MdHomeFilled />
+        </NavLink>
+      </div>
     </div>
   );
 };
